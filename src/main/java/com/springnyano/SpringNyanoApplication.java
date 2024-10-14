80b772c22839728fb4d44002c89801323c72c8a0
nhh57 <nguyenhoanghai0507@gmail.com>
2024-10-14 10:55:36 +0700

    first commit

---
+++ src/main/java/com/springnyano/SpringNyanoApplication.java
@@ -0,0 +1,13 @@
+package com.springnyano;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringNyanoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringNyanoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/springnyano/SpringNyanoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springnyano;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringNyanoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
